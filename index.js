db0cfacba48e78a55af6cc5b786cc268206821ed
ana.rajner@gmail.com <ana.rajner@gmail.com>
2018-03-15 13:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object,{[key]: value});
 }
+
+function deleteFromObjectByKey(object,key) {
+  var newOb = {object};
+  delete newOb.key;
+  return newOb;
+}
